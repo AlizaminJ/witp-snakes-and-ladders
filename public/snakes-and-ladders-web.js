308e34abb3c3181473e0c38abacf5fd8402a59e9
mberkay09 <berkaybayhan@hotmail.com>
2019-01-17 22:06:28 +0100

    rending the board

--- public/snakes-and-ladders-web.js
+++ public/snakes-and-ladders-web.js
@@ -1,5 +1,30 @@
 'use strict';
 function main() {
     console.log("main is working");
+    createBoard();
 }
-window.onload = main;
+function createBoard() {
+    let table = "";
+    let cellNumber = 1;
+    for (let i = 0; i < 10; i++) {
+        table += "<tr>";
+        for (let j = 0; j < 10; j++) {
+
+            table += "<td id=" + cellNumber + ">";
+            table += cellNumber++ + "</td>";
+        }
+        table += "</tr>"
+    }
+    document.getElementById("board").innerHTML = table;
+
+    $(function () {
+        $("tbody").each(function () {
+            var arr = $.makeArray($("tr", this).detach());
+            arr.reverse();
+
+            $(this).append(arr);
+
+        });
+    });
+}
+    window.onload = main;
