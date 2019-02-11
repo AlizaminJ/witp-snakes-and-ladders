cb6cbf69d44394eac9d1ee10d0ed0d259ba16a35
nayyarawan <nayyar.awan@gmail.com>
2019-02-11 02:53:19 +0100

    update

--- public/snakes-and-ladders-web.js
+++ public/snakes-and-ladders-web.js
@@ -2,6 +2,7 @@
 import {move} from "./features.js";
 
 function main() {
+
     console.log("main is working");
     //createBoard();
 
@@ -12,24 +13,22 @@ renderBoard();
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-
+let cells = [];
+let coinCurrentPosition = 1;
+let isBoardRenderdOnce = false;
 function renderBoard() {
-
+    // intializing the neccessary variables
     let row=10;
     let col=10;
     let cellWidth = c.width/10;
-
     let cellHeight = c.height/10;
     let cellStartY = 0;
     let cellStartX = 0;
     let cellNumber = 100;
     let evenRow = false;
-    //let tempeven = false;
-
+    // loop for render the board
     for (let i=0;i<row;i++)
     {
-
-       // cellStartX = 0;
         if (evenRow)
         {
             cellStartX = c.width;
@@ -45,37 +44,77 @@ function renderBoard() {
                 ctx.rect(cellStartX, cellStartY, cellWidth,cellHeight);
                 ctx.fillText(cellNumber, cellStartX+20, cellStartY+20);
                 ctx.stroke();
+
             }
             else
             {
                 ctx.rect(cellStartX, cellStartY, cellWidth,cellHeight);
                 ctx.fillText(cellNumber, cellStartX+20, cellStartY+20);
                 ctx.stroke();
+
+            }
+            if (!isBoardRenderdOnce) {
+                let cell = {
+                    cellNum: cellNumber,
+                    xAxis: cellStartX,
+                    yAxis: cellStartY
+                }
+                cells.push(cell);
+            }
+            if (!evenRow) {
                 cellStartX += cellWidth;
             }
 
             cellNumber--;
 
         }
+
         cellStartY += cellHeight;
         evenRow = !evenRow;
     }
 
+    isBoardRenderdOnce = true;
+
 
 }
-placeCoin(10,10);
-function placeCoin(x,y) {
+ctx.fillStyle = "red";
+ctx.fillRect(0 + 13,585 +22, 40, 40);
+
+function moveCoin(nextPos, diceVal) {
+    var lastPos = nextPos;
+    nextPos += diceVal;
+    var temp = lastPos;
+    lastPos = nextPos - lastPos;
+
+    for (let i=0;i<lastPos;i++) {
+        if (nextPos < 101) {
+            var currentCell = cells.filter(c => c.cellNum == temp);
+            if (currentCell) {
+                coinCurrentPosition = nextPos;
+            }
+            setTimeout(function () {
+                ctx.clearRect(0, 0, c.width, c.height);
+                renderBoard();
+                ctx.fillStyle = "red";
+                ctx.fillRect(currentCell[0].xAxis + 13, currentCell[0].yAxis + 22, 40, 40);
+            },10);
 
 
-    ctx.fillStyle = "red";
-    ctx.rect(x,y, 40, 40);
-//ctx.stroke();
+        }
+        temp++;
+    }
+}
+function rollDice(){
+    let x = Math.floor(Math.random() * 6) + 1;
+    return x;
 }
 
 document.getElementById("dice").addEventListener("click", function () {
-   console.log("hi");
-    placeCoin(100,10);
+    var diceVal = rollDice();
+    document.getElementById("output").innerHTML = diceVal;
+    setInterval(moveCoin(coinCurrentPosition, diceVal),1000);
 });
+
 function createBoard() {
     let table = "";
     let cellNumber = 1;
